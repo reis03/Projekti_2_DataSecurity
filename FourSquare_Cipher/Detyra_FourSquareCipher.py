ec984254fd1bc7faf3ddbf0c3ca0d2431c555324
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2023-04-15 18:07:14 +0200

    Update Detyra_FourSquareCipher.py

--- FourSquare_Cipher/Detyra_FourSquareCipher.py
+++ FourSquare_Cipher/Detyra_FourSquareCipher.py
@@ -37,6 +37,49 @@ def makeMatrix(key): #creates a char array to be used as a matrix
     return ''.join(matrix)
 
 
+def printMatrix(matrix): #prints the char array as a matrix
+
+    counter = 0
+    for x in range(5):
+        print("\n")
+        for y in range(5):
+            print (matrix[counter], end=' ')  # end='' --> vetem rregullim i paraqitjes ne menyre horizontale
+            counter += 1
+    print("\n\n")
+
+
+
+def removeDuplicates(str): #removes duplicates from keys
+    result=[]
+    seen=set()
+    for char in str:
+        if char not in seen:
+            seen.add(char)
+            result.append(char)
+    return ''.join(result)
+
+
+
+#evaluate function: used to determine which index position on the
+#    mirror matrix the key is located
+def evaluate(ref1, ref2):
+    return ((int(ref1 / 5) * 5) + (ref2 % 5))
+
+
+
+
+#searches for the position index of the message letter in the ref matrix
+def search (ref, letter):
+    counter = 0
+    if letter == 'Z':
+        return -1
+    for char in ref:
+        if ref[counter] == letter:
+            return counter
+        counter += 1
+    pass
+
+
 def encrypt(message , key1, key2):
 
 
